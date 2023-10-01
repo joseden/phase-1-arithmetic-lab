aa1e9df1aca0f150d1b99ae2a86a69bc8b8864ab
Joseph <joseph.dena@student.moringaschool.com>
2023-10-01 17:49:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+let num1 = 2
+let num2 = 31
+ 
+let multiply = num1 * num2;
 
-// Write your code here
+
+const random =  Math.floor(Math.random() * 100) + 2;
+
+console.log(random);
+
+
+
+let num3 = 73
+let num4 = 69
+
+const mod = num3-num4;
+console.log(mod);
+
+
+const max = Math.max(1, 2, 3, 4, 20);
+console.log(max);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
